18d83a46aa496db9b5ac638b66f0f75891154b01
Alexandr Varygin <otz-job@mail.ru>
2020-10-25 12:22:15 +0300

    delete comments from index.php

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
   require_once "Database.php";
 
-  // $users = Database::getInstance()->query("SELECT * FROM users WHERE username = ?", ['user1']);
-  // $users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?, ?)", ['user1', 'user2']);
-  // $users = Database::getInstance()->get('users', ['username', '=', 'user1']);
-  // $users = Database::getInstance()->get('users', ['password', '=', 'password']);
-
-  // Database::getInstance()->action('DELETE', 'users', ['username', '=', 'user1']);
   $users = Database::getInstance()->get('users', ['password', '=', 'password3']);
-
-  //var_dump($users->count());
-  // Database::getInstance()->get('users', ['username', '=', 'user1']);
-  // Database::getInstance()->delete('users', ['username', '=', 'user1']);
 
   if ($users->error()) {
     echo 'we have an error';
