7ff99937a73e94adb69d92c5d2879f62bda1fc73
Alexandr Varygin <otz-job@mail.ru>
2020-10-28 19:58:25 +0300

    add function insert

--- Database.php
+++ Database.php
@@ -97,5 +97,24 @@
 
           return false;
       }
+
+      public function insert($table, $fields = [])
+      {
+        $values = '';
+        foreach ($fields as $field) {
+          $values .= "?,";
+        }
+
+        $values = rtrim($values, ',');
+
+//        $sql = "INSERT INTO {$table} (`" . implode('`, `',array_keys($fields)) . "`) VALUES (" . $values . ")";
+        $sql = "INSERT INTO {$table} (" . '`' . implode('`, `', array_keys($fields)) . '`' . ") VALUES ({$values})";
+
+        if (!$this->query($sql, $fields)->error()) {
+          return true;
+        }
+
+        return false;
+      }
   }
 ?>
--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php
   require_once "Database.php";
 
-  $users = Database::getInstance()->get('users', ['password', '=', 'password3']);
+//  $users = Database::getInstance()->get('users', ['password', '=', 'password3']);
+  Database::getInstance()->insert('users', [
+    'username' => 'Marlin',
+    'password' => 'password'
+  ]);
 
-  if ($users->error()) {
-    echo 'we have an error';
-  } else {
-     foreach ($users->results() as $user) {
-      echo $user->username . '<br>';
-     }
-  }
+//  if ($users->error()) {
+//    echo 'we have an error';
+//  } else {
+//     foreach ($users->results() as $user) {
+//      echo $user->username . '<br>';
+//     }
+//  }
   
 ?>
