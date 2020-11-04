588d39790da855ca89e1699de380ee28c21320bc
Alexandr Varygin <otz-job@mail.ru>
2020-11-04 21:44:20 +0300

    remember me

--- classes/User.php
+++ classes/User.php
@@ -1,11 +1,12 @@
 <?php
 
 class User {
-  private $db, $data, $session_name, $isLoggedIn;
+  private $db, $data, $session_name, $isLoggedIn, $cookieName;
 
   public function __construct($user = null) {
     $this->db = Database::getInstance();
     $this->session_name = Config::get('session.user_session');
+    $this->cookieName = Config::get('cookie.cookie_name');
 
     if(!$user) {
       if(Session::exists($this->session_name)) {
@@ -23,20 +24,39 @@ class User {
   }
 
   public function create($fields = []) {
-//    var_dump($fields);die();
     $this->db->insert('users', $fields);
   }
 
-  public function login($email = null, $password = null) {
-    if($email) {
-      $user = $this->find($email);
+  public function login($email = null, $password = null, $remember = false) {
+    if(!$email && !$password && $this->exists()) {
+        Session::put($this->session_name, $this->data()->id);
+    } else {
+        $user = $this->find($email);
+        if($user) {
+            if(password_verify($password, $this->data()->password)) {
+                Session::put($this->session_name, $this->data()->id);
 
-      if($user) {
-        if(password_verify($password, $this->data()->password)) {
-          Session::put($this->session_name, $this->data()->id);
-          return true;
+                if($remember) {
+                    $hash = hash('sha256', uniqid());
+
+                    $hashCheck = $this->db->get('user_sessions', ['user_id', '=', $this->data()->id]);
+
+                    if(!$hashCheck->count()) {
+                        $this->db->insert('user_sessions', [
+                            'user_id' => $this->data()->id,
+                            'hash' => $hash
+                        ]);
+                    } else {
+                        $hash = $hashCheck->first()->hash;
+                    }
+
+                    Cookie::put($this->cookieName, $hash, Config::get('cookie.cookie_expiry'));
+
+                }
+
+                return true;
+            }
         }
-      }
     }
 
     return false;
@@ -65,6 +85,41 @@ class User {
   }
 
   public function logout() {
-      return Session::delete($this->session_name);
+//      return Session::delete($this->session_name);
+      $this->db->delete('user_sessions', ['user_id', '=', $this->data()->id]);
+      Session::delete($this->session_name);
+      Cookie::delete($this->cookieName);
   }
+
+  public function exists() {
+      return (!empty($this->data())) ? true : false;
+  }
+
+  public function update($fields = [], $id = null) {
+
+      if(!id && $this->isLoggedIn()) {
+          $id = $this->data()->id;
+      }
+
+      $this->db->update('users', $id, $fields);
+  }
+
+  public function hasPermissions($key = null) {
+
+      if($key) {
+          $group = $this->db->get('groups', ['id', '=', $this->data()->group_id]);
+
+          if($group->count()) {
+              $permissions = $group->first()->permissions;
+              $permissions = json_decode($permissions, true);
+
+              if($permissions[$key]) {
+                  return true;
+              }
+          }
+      }
+
+      return false;
+  }
+
 }
--- index.php
+++ index.php
@@ -6,11 +6,7 @@ require_once 'init.php';
 //var_dump(Session::get(Config::get('session.user_session')));
 
 $user = new User;
-$user12 = new User(12);
-echo $user->data()->username;
-echo '<br>';
-var_dump($user12->data()->username);
-echo '<br>';
+
 if($user->isLoggedIn()) {
     echo "Hi, <a href='#'>{$user->data()->username}</a>!";
     echo "<p><a href='logout.php'>Logout</a></p>";
